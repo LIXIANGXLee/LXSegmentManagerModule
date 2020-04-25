b5903bc7ad1c0f83b2ea3c142289eadaf83cb3bf
lixiang <lixiang@gokuaidian.com>
2020-04-25 18:58:35 +0800

    Initial Commit

---
+++ LXSegmentManagerModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LXSegmentManagerModule
+//
+//  Created by Mac on 2020/4/25.
+//  Copyright © 2020 李响. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
